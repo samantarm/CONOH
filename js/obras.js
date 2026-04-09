e2f605a0fca1beb0a77175b0f1b490e98a961529
Lucas <lucas.moudden@live.u-tad.com>
2026-04-09 16:33:31 +0100

    eprogresovital

--- js/obras.js
+++ js/obras.js
@@ -1,5 +1,14 @@
 
 // Encontrar los elementos HTML a cambiar
+var img = document.getElementById("img");
+var titulo = document.getElementById("titulo");
+var autor = document.getElementById("autor");
+var categoria = document.getElementById("categoria");
+var fecha = document.getElementById("fecha");
+var localizacion = document.getElementById("localizacion");
+var desc = document.getElementById("desc");
+var tags = document.getElementById("tags");
+
 const container = document.getElementById('obrasinfo');
 var datos = null;
 
@@ -21,7 +30,16 @@ fetch("/json/obra.json")
 
 function init () {
     // obtener el id que viene en la URL
+URLSearchParams
     // BUSCAR en datos la obra que corresponde a ese ID
-    search
+    
     // Cambiar el contenido de los elementos HTML con los datos de la obra
+    img.src = obra.Photo;
+    titulo.innerText = obra.Title;
+    autor.innerText = obra.Author;
+    categoria.innerText = obra.Category;
+    fecha.innerText = obra.Date;
+    localizacion.innerText = obra.Location;
+    desc.innerText = obra.Description;
+    tags.innerText = obra.Tags;
 }
